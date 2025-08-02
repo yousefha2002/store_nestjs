b727a75270f425983692425d6563afa64f48e35e
yousefha2002 <yousefha2029@gmail.com>
2025-08-02 10:23:27 +0300

    login customer

--- src/modules/customer/customer.controller.ts
+++ src/modules/customer/customer.controller.ts
@@ -5,6 +5,7 @@ import { multerOptions } from 'src/multer/multer.options';
 import { MulterExceptionFilter } from 'src/multer/multer.exception.filter';
 import { CreateCustomerDto } from './dto/create-customer.dto';
 import { Language } from 'src/common/enums/language';
+import { LoginCustomerDto } from './dto/login-customer.dto';
 
 @Controller('customer')
 export class CustomerController {
@@ -16,5 +17,10 @@ export class CustomerController {
   createCustomer(@Body() body:CreateCustomerDto,@Query('lang') lang=Language.en,@UploadedFile() file?: Express.Multer.File)
   {
     return this.customerService.createCustomer(body,lang,file)
+  }
+
+  @Post('login')
+  async loginOwner(@Body() body: LoginCustomerDto,@Query('lang') lang:Language.en) {
+    return this.customerService.login(body,lang);
   }
 }
