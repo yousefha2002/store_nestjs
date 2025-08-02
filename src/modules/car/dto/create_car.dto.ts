87f673e6773e7630e20807282ce5b0a3ebee0d05
moh7amed2002 <moha7medheshamabohani@gmail.com>
2025-08-02 12:50:48 +0300

    car color

--- src/modules/car/dto/create_car.dto.ts
+++ src/modules/car/dto/create_car.dto.ts
@@ -1,38 +1,45 @@
-import { IsBoolean, IsNotEmpty, IsNumber, IsOptional, IsString, MaxLength, MinLength } from 'class-validator';
+import {
+  IsBoolean,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 
 export class CreateCarDto {
+  @MinLength(3, { message: 'Name must be at least 3 characters' })
+  @MaxLength(20, { message: 'Name must be at most 20 characters' })
+  @IsString()
+  @IsNotEmpty()
+  carName: string;
 
-    @MinLength(3, { message: 'Name must be at least 3 characters' })
-    @MaxLength(20, { message: 'Name must be at most 20 characters' })
-    @IsString()
-    @IsNotEmpty()
-    carName: string;
+  @IsNumber()
+  @IsNotEmpty()
+  carTypeId: number;
 
-    @IsNumber()
-    @IsNotEmpty()
-    carTypeId: number;
+  @IsString()
+  @IsNotEmpty()
+  color: string;
 
-    @IsNumber()
-    @IsNotEmpty()
-    colorId: number;
+  @IsNumber()
+  @IsNotEmpty()
+  brandId: number;
 
-    @IsNumber()
-    @IsNotEmpty()
-    brandId: number;
+  @IsNumber()
+  @IsNotEmpty()
+  modelId: number;
 
-    @IsNumber()
-    @IsNotEmpty()
-    modelId: number;
+  @IsString()
+  @IsOptional()
+  plateNumber?: string;
 
-    @IsString()
-    @IsOptional()
-    plateNumber?: string;
+  @IsString()
+  @IsOptional()
+  plateLetters?: string;
 
-    @IsString()
-    @IsOptional()
-    plateLetters?: string;
-
-    @IsBoolean()
-    @IsOptional()
-    isDefault?: boolean;
+  @IsBoolean()
+  @IsOptional()
+  isDefault?: boolean;
 }
--- src/modules/car/entities/car.entity.ts
+++ src/modules/car/entities/car.entity.ts
@@ -11,7 +11,6 @@ import {
   Default,
 } from 'sequelize-typescript';
 import { CarBrand } from 'src/modules/car_brand/entities/car_brand.entity';
-import { CarColor } from 'src/modules/car_color/entities/car_color.entity';
 import { CarModel } from 'src/modules/car_model/entites/car_model.entity';
 import { CarType } from 'src/modules/car_type/entites/car_type.entity';
 
@@ -19,64 +18,60 @@ import { Customer } from 'src/modules/customer/entities/customer.entity';
 
 @Table({ tableName: 'cars' })
 export class Car extends Model {
-    @AutoIncrement
-    @PrimaryKey
-    @Column(DataType.INTEGER)
-    id: number;
+  @AutoIncrement
+  @PrimaryKey
+  @Column(DataType.INTEGER)
+  id: number;
 
-    @ForeignKey(() => Customer)
-    @AllowNull(false)
-    @Column(DataType.INTEGER)
-    customerId: number;
+  @ForeignKey(() => Customer)
+  @AllowNull(false)
+  @Column(DataType.INTEGER)
+  customerId: number;
 
-    @BelongsTo(() => Customer)
-    customer: Customer;
+  @BelongsTo(() => Customer)
+  customer: Customer;
 
-    @AllowNull(false)
-    @Column(DataType.STRING)
-    carName: string;
+  @AllowNull(false)
+  @Column(DataType.STRING)
+  carName: string;
 
-    @ForeignKey(() => CarType)
-    @AllowNull(false)
-    @Column(DataType.INTEGER)
-    carTypeId: number;
+  @ForeignKey(() => CarType)
+  @AllowNull(false)
+  @Column(DataType.INTEGER)
+  carTypeId: number;
 
-    @BelongsTo(() => CarType)
-    carType: CarType;
+  @BelongsTo(() => CarType)
+  carType: CarType;
 
-    @ForeignKey(() => CarColor)
-    @AllowNull(false)
-    @Column(DataType.INTEGER)
-    colorId: number;
+  @AllowNull(false)
+  @Column(DataType.STRING)
+  color: string;
 
-    @BelongsTo(() => CarColor)
-    color: CarColor;
+  @ForeignKey(() => CarBrand)
+  @AllowNull(false)
+  @Column(DataType.INTEGER)
+  brandId: number;
 
-    @ForeignKey(() => CarBrand)
-    @AllowNull(false)
-    @Column(DataType.INTEGER)
-    brandId: number;
+  @BelongsTo(() => CarBrand)
+  brand: CarBrand;
 
-    @BelongsTo(() => CarBrand)
-    brand: CarBrand;
+  @ForeignKey(() => CarModel)
+  @AllowNull(false)
+  @Column(DataType.INTEGER)
+  modelId: number;
 
-    @ForeignKey(() => CarModel)
-    @AllowNull(false)
-    @Column(DataType.INTEGER)
-    modelId: number;
+  @BelongsTo(() => CarModel)
+  model: CarModel;
 
-    @BelongsTo(() => CarModel)
-    model: CarModel;
+  @AllowNull(true)
+  @Column(DataType.STRING)
+  plateNumber: string;
 
-    @AllowNull(true)
-    @Column(DataType.STRING)
-    plateNumber: string;
+  @AllowNull(true)
+  @Column(DataType.STRING)
+  plateLetters: string;
 
-    @AllowNull(true)
-    @Column(DataType.STRING)
-    plateLetters: string;
-
-    @Default(false)
-    @Column(DataType.BOOLEAN)
-    isDefault: boolean;
+  @Default(false)
+  @Column(DataType.BOOLEAN)
+  isDefault: boolean;
 }
